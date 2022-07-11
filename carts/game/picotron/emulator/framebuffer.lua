6c8504cf85e64191e11fb2106536bd6e9a66d59b
freds72 <freds72@users.noreply.github.com>
2022-07-11 21:00:04 +0200

    screen wobbling (#14)
    * basic death sequence
    
    * added damage to doors & plats
    
    * added bounce physics (wip)
    
    * basic health support
    improved death anim
    
    * fixed bogus mins/maxs collision hull
    fixed bogus platform trigger field
    
    * fixed linked door detection
    
    * fixed mins/maxs for secret doors
    
    * fixed bogus epsilon clipping
    
    * fixed level transition time
    fixed bounce/toss gravity
    
    * basic pain feedback
    lower eyepos when dead!
    
    * added water+slime damage
    
    * added screen wobbling under surface
    
    Co-authored-by: Frederic Souchu <frederic.souchu@worldline.com>

--- carts/game/picotron/emulator/framebuffer.lua
+++ carts/game/picotron/emulator/framebuffer.lua
@@ -7,6 +7,8 @@ local vid_ptr = display.buf[0]
 
 function FrameBuffer:present(x,y,data,pal,scale)
     for i=0,len-1 do
+        --local r=math.min(bit.band(pal[data[i]],0xff)+0x40,0xff)
+        -- vid_ptr[i]=bit.bor(r,bit.band(pal[data[i]],0xffffff00))
         vid_ptr[i]=pal[data[i]]
     end
 
