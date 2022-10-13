207c55731a5dddd51faf57927ec811e9c921e63b
freds <frederic.souchu@laposte.net>
2022-10-13 20:43:11 +0200

    fixed close polygons rendering precision

--- carts/game/renderer/span_rasterizer.lua
+++ carts/game/renderer/span_rasterizer.lua
@@ -98,7 +98,9 @@ local function spanfill(x0,x1,y,u,v,w,du,dv,dw,fn)
 			local dx,sdw=x0-s0,_ptr[span+3]
 			local sw=_ptr[span+2]+dx*sdw		
 			
-			if sw-w<-0.00001 or (sw-w<0.00001 and dw>sdw) then
+			-- use scaled precision for abutting spans (see Christer Ericson)
+			-- use absolute distance for other planes
+			if sw-w<-0.00001 or (abs(sw-w)<=1e-5*max(abs(sw),max(abs(w),1)) and dw>sdw) then
 				--printh(sw.."("..dx..") "..w.." w:"..span.dw.."<="..dw)	
 				-- insert (left) clipped existing span as a "new" span
 				if dx>0 then
